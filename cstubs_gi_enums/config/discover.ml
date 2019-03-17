057f519d676b21e0fdc20d21e1a2d9f4b676e260
Cédric Le Moigne <cedlemo@gmx.com>
2019-03-17 10:30:45 +0100

    Clean discover.ml

--- cstubs_gi_enums/config/discover.ml
+++ cstubs_gi_enums/config/discover.ml
@@ -2,8 +2,13 @@ open Base
 open Stdio
 module C = Configurator
 
-let write_sexp fn sexp =
-  Out_channel.write_all fn ~data:(Sexp.to_string sexp)
+let write_sexp fn list_of_str =
+  let data = sexp_of_list sexp_of_string list_of_str |> Sexp.to_string in
+  Out_channel.write_all fn ~data
+
+let write_flags file list_of_str =
+  let data = String.concat list_of_str ~sep:" " in
+  Out_channel.write_all file ~data
 
 let () =
   C.main ~name:"GObject-Introspection" (fun c ->
@@ -28,16 +33,21 @@ let () =
       match C.Pkg_config.get c with
       | None -> default
       | Some pc ->
-         let libffi = Option.value (C.Pkg_config.query pc ~package:"libffi") ~default:default_ffi in
-         let gobject = Option.value (C.Pkg_config.query pc ~package:"gobject-introspection-1.0") ~default in
+         let get_config package default =
+           Option.value (C.Pkg_config.query pc ~package) ~default in
+         let libffi = get_config "libffi" default_ffi in
+         let gobject = get_config "gobject-introspection-1.0" default in
          let  module P = C.Pkg_config in
          { libs = (libffi.P.libs @ gobject.P.libs);
            cflags = (libffi.P.cflags @ gobject.P.cflags) }
     in
     let os_type = C.ocaml_config_var_exn (C.create "") "system" in
-    let ccopts = if Base.String.(os_type = "macosx") then [""] else ["-Wl,-no-as-needed"] in
-    write_sexp "c_flags.sexp"         (sexp_of_list sexp_of_string conf.cflags);
-    write_sexp "c_library_flags.sexp" (sexp_of_list sexp_of_string conf.libs);
-    write_sexp "ccopts.sexp" (sexp_of_list sexp_of_string ccopts);
-    Out_channel.write_all "gi-cclib" ~data:(String.concat conf.libs ~sep:" ");
-    Out_channel.write_all "gi-ccopt" ~data:(String.concat conf.cflags ~sep:" "))
+    let ccopts =
+      if Base.String.(os_type = "macosx") then [""]
+      else ["-Wl,-no-as-needed"]
+    in
+    write_sexp "c_flags.sexp"         conf.cflags;
+    write_sexp "c_library_flags.sexp" conf.libs;
+    write_sexp "ccopts.sexp"          ccopts;
+    write_flags "gi-cclib"            conf.libs;
+    write_flags "gi-ccopt"            conf.cflags)
